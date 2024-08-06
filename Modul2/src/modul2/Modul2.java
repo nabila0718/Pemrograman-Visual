7cc20d307e2083e81b3cc85f2de1d62353afde4c
Nabila Sri Yulianti <119402611+nabila0718@users.noreply.github.com>
2024-08-06 15:28:30 +0700

    Modul 2 - Pemrograman Visual

---
+++ Modul2/src/modul2/Modul2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Tugas1 modul=new Tugas1();
+        //modul.setVisible(true);
+   
+        //Tugas2 modul=new Tugas2();
+        //modul.setVisible(true);
+        
+        Tugas3 modul=new Tugas3();
+        modul.setVisible(true);
+        
+    }
+    
+}
